ab741289405aa2bfebf8b9ca47e3d401e7a61ad0
tradaduong2811 <htluan2811@gmail.com>
2015-12-11 15:26:56 +0700

    Xử lý
    - Auth, Đăng nhập OToken

---
+++ Source/AngularJSAuthentication.API/Models/Members.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJSAuthentication.API.Models
+{
+    public class Members
+    {
+        public int MemberId { get; set; }
+        public string UserId { get; set; }
+        public string Fullname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string University { get; set; }
+    }
+}
---
+++ Source/ReadMeFirst.txt
@@ -0,0 +1,7 @@
+﻿- Mở web.config bên back-end, đổi tên database thành KNSERDB
+- Trong Console enable migration và update-database
+
+
+- Đặt tên Solution mới là: KNSER
+- Tên Project Back-end (Empty, chọn WebAPI): Handle_KNSER
+- Tên Project Front-end (MVC, KHÔNG chọn WebAPI): Web_KNSER
