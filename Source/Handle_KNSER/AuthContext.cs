f4a9d2475a7be3aa2fcd048a715cf2a723a892f4
Nam Pham <nampham7394@gmail.com>
2015-12-14 16:09:11 +0700

    Changed database
    -Auth Owin login
    - Facebook login (20%)

---
+++ Source/Handle_KNSER/AuthContext.cs
@@ -0,0 +1,23 @@
+﻿using Handle_KNSER.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Handle_KNSER
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("KNSERDB")
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+
+}
