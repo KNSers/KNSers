3081ad6f56c9093040fbc0abdc984b4499e04f07
tradaduong2811 <htluan2811@gmail.com>
2015-12-20 16:51:40 +0700

    update database
    update database

--- Source/Handle_KNSER/AuthContext.cs
+++ Source/Handle_KNSER/AuthContext.cs
@@ -18,6 +18,13 @@ namespace Handle_KNSER
 
         public DbSet<Client> Clients { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Letter> Letters { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+
     }
 
 }
