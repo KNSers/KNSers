6fc5c983ea0c37f2649aa787874921e9e4293673
Nam Pham <nampham7394@gmail.com>
2015-12-26 09:20:52 +0700

    change

--- Source/Handle_KNSER/Hubs/NotificationHub.cs
+++ Source/Handle_KNSER/Hubs/NotificationHub.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Microsoft.AspNet.SignalR;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
 
 namespace Handle_KNSER.Hubs
 {
     public class NotificationHub : Hub
     {
-        public void Hello()
+      
+        public string  UserName { get; set; }
+        public string Message { get; set; }
+
+        public async Task NewPeopleJoinEvent(string name, string message)
         {
-            Clients.All.hello();
+            Clients.All.addNotificationJoinEventToPage(new NotificationHub() { UserName = name, Message = message });
         }
     }
 }
